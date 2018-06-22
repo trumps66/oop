a419fa147a68f3d92912539fb28c6db9785fc0fa
11 <a11@11s-Mac-mini.local>
2018-06-22 23:00:59 +0800

    Initial commit

---
+++ 01.py
@@ -0,0 +1,18 @@
+class Student():
+    pass
+
+liyi = Student()
+
+class PythonStudent():
+    name = None
+    age = 18
+    course = 'Python'
+
+    def doHomework(self):
+        print('I 在做作业')
+        return None
+
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.age)
+yueyue.doHomework()
